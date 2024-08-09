728f1887f769b378a498d1dd29ed9661323ab2ca
Zalán Bálint Lévai <zalan.levai@gmail.com>
2024-08-09 14:58:36 +0100

    test(bool_expr_negate): Add UI test cases

---
+++ tests/ui/mutation/ops/bool_expr_negate/negate_bool_exprs.rs
@@ -0,0 +1,39 @@
+//@ print-mutants
+//@ build
+//@ stdout
+//@ stderr: empty
+//@ mutation-operators: bool_expr_negate
+
+fn f() {
+    let not_a_bool = 5 + 7 - 3;
+
+    let mut tainted = false;
+    for i in 0..10 {
+        if i > 9 {
+            tainted = true;
+        }
+    }
+
+    let complex_bool = (not_a_bool + 1) == 10 || not_a_bool * 0 >= 1;
+
+    if tainted && not_a_bool == 9 {
+        println!("tainted && not_a_bool == 9");
+    } else if tainted && not_a_bool > 9 {
+        println!("tainted && not_a_bool > 9");
+    } else if !complex_bool {
+        println!("!complex_bool");
+    } else {
+        println!("something else");
+    }
+
+    match () {
+        _ if complex_bool => { println!("complex_bool"); }
+        () if not_a_bool == 9 || tainted => { println!("not_a_bool == 9 || tainted"); }
+        _ => { println!("something else"); }
+    }
+}
+
+#[test]
+fn test() {
+    f();
+}
---
+++ tests/ui/mutation/ops/bool_expr_negate/remove_negation_from_negated_exprs.rs
@@ -0,0 +1,44 @@
+//@ print-mutants
+//@ build
+//@ stdout
+//@ stderr: empty
+//@ mutation-operators: bool_expr_negate
+
+fn f() {
+    let not_a_bool = 5 + 7 - 3;
+
+    let mut tainted = !true;
+    for i in 0..10 {
+        if !(i <= 9) {
+            tainted = !false;
+        }
+    }
+
+    let complex_bool = !((not_a_bool + 1) != 10) || !(not_a_bool * 0 < 1);
+
+    if tainted && !(not_a_bool != 9) {
+        println!("tainted && not_a_bool == 9");
+    } else if tainted && !(not_a_bool <= 9) {
+        println!("tainted && not_a_bool > 9");
+    } else if !!!complex_bool {
+        println!("!complex_bool");
+    } else {
+        println!("something else");
+    }
+
+    match () {
+        _ if !!complex_bool => { println!("complex_bool"); }
+        () if !(not_a_bool != 9) || tainted => { println!("not_a_bool == 9 || tainted"); }
+        _ => { println!("something else"); }
+    }
+
+    let nested_ident_negation = !!!complex_bool;
+    let nested_expr_negation = !!!!(!(not_a_bool == 9));
+
+    let _ = nested_ident_negation || nested_expr_negation;
+}
+
+#[test]
+fn test() {
+    f();
+}
