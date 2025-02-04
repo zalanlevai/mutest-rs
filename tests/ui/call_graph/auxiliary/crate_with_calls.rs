8453d9fee0405956e52ce60e74de3023dcd89c1a
Zalán Bálint Lévai <zalan.levai@gmail.com>
2025-02-04 16:33:28 +0000

    test(call_graph): Add MIR-less call to harden extern crate call test
    Extern function callees are discovered through their MIR metadata entries, which are not present for all items. For example, private, non-generic functions do not have their MIRs saved into the crate metadata for efficiency. This is fine, because such cases cannot call back to the local crate, which is what we are ultimately looking for.
    
    This test case addition ensures that we handle the lack of MIR availability during call graph construction gracefully.

--- tests/ui/call_graph/auxiliary/crate_with_calls.rs
+++ tests/ui/call_graph/auxiliary/crate_with_calls.rs
@@ -4,6 +4,13 @@ pub trait ExternTrait {
     fn extern_trait_fn();
 }
 
+fn extern_private_fn() {}
+
 pub fn extern_fn_calling_trait_fn<T: ExternTrait>() {
     <T as ExternTrait>::extern_trait_fn();
+
+    // NOTE: This function call is here to represent any calls in extern functions that point to
+    //       functions with no encoded MIR form available (e.g. private, non-generic functions)
+    //       See `rustc_metadata::rmeta::encoder::should_encode_mir` for more details.
+    extern_private_fn();
 }
